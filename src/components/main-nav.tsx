e9b1f349abb59d4d6284723b380a8691ce9bc1d9
gautchap <gautchap+github@outlook.com>
2023-11-08 01:34:10 +0100

    refactor : use theme toggle in nav

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,73 +1,78 @@
+"use client";
+
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
-import { ThemeToggle } from "@/components/ThemeToggle";
-import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export function MainNav() {
-    return (
-        <div className="hidden md:flex md:w-full md:items-center md:justify-between">
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <Link href="#gaut" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                <b>&lt;ggaut.dev&gt;</b>
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <Link href="#projects" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                &lt;/projects&gt;
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="#about" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                &lt;/about&gt;
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="#contact" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                &lt;/contact&gt;
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <div className="flex pl-5">
-                        <NavigationMenuItem>
-                            <Button variant="ghost" size="icon">
-                                <Link target="_blank" href="https://github.com/gautchap">
-                                    <Icons.gitHub className="h-[1.2rem] w-[1.2rem]" />
-                                </Link>
-                            </Button>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Button variant="ghost" size="icon">
-                                <Link target="_blank" href="https://linkedin.com/in/gauthier-chapon">
-                                    <Icons.linkedin className="h-[1.2rem] w-[1.2rem]" />
-                                </Link>
-                            </Button>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <ThemeToggle />
-                        </NavigationMenuItem>
-                    </div>
-                </NavigationMenuList>
-            </NavigationMenu>
-        </div>
-    );
+  return (
+    <div className="hidden md:flex md:w-full md:items-center md:justify-between">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="#gaut" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <b>&lt;ggaut.dev&gt;</b>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="#about" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                &lt;/about&gt;
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="#projects" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                &lt;/projects&gt;
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="#contact" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                &lt;/contact&gt;
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <div className="flex pl-5">
+            <NavigationMenuItem>
+              <Button variant="ghost" size="icon">
+                <Link target="_blank" href="https://github.com/gautchap">
+                  <Icons.gitHub className="h-[1.2rem] w-[1.2rem]" />
+                </Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button variant="ghost" size="icon">
+                <Link
+                  target="_blank"
+                  href="https://linkedin.com/in/gauthier-chapon"
+                >
+                  <Icons.linkedin className="h-[1.2rem] w-[1.2rem]" />
+                </Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <ThemeToggle />
+            </NavigationMenuItem>
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 }
--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -1,60 +1,70 @@
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Link from "next/link";
-import { ThemeToggle } from "@/components/ThemeToggle";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export function SideNav() {
-    return (
-        <div className="flex w-full items-center justify-between md:hidden">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <button className="h-5 w-5">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="1em"
-                            viewBox="0 0 448 512"
-                            className="fill-current bg-background text-foreground h-full w-full"
-                        >
-                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                        </svg>
-                    </button>
-                </SheetTrigger>
-                <SheetContent side="top" className="p-10">
-                    <SheetHeader className="text-left">
-                        <SheetTitle>
-                            <SheetClose asChild>
-                                <Link href="#gaut">&lt;ggaut.dev&gt;</Link>
-                            </SheetClose>
-                        </SheetTitle>
-                        <hr />
-                    </SheetHeader>
-                    <div className="flex flex-col gap-2 pt-2">
-                        <SheetClose asChild>
-                            <Link href="#projects">&lt;/projects&gt;</Link>
-                        </SheetClose>
-                        <SheetClose asChild>
-                            <Link href="#about">&lt;/about&gt;</Link>
-                        </SheetClose>
-                        <SheetClose asChild>
-                            <Link href="#contact">&lt;/contact&gt;</Link>
-                        </SheetClose>
-                    </div>
-                </SheetContent>
-            </Sheet>
-            <div>
-                <Button variant="ghost" size="icon">
-                    <Link target="_blank" href="https://github.com/gautchap">
-                        <Icons.gitHub className="h-[1.2rem] w-[1.2rem]" />
-                    </Link>
-                </Button>
-                <Button variant="ghost" size="icon">
-                    <Link target="_blank" href="https://www.linkedin.com/in/gauthier-chapon">
-                        <Icons.linkedin className="h-[1.2rem] w-[1.2rem]" />
-                    </Link>
-                </Button>
-                <ThemeToggle />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex w-full items-center justify-between md:hidden">
+      <Sheet>
+        <SheetTrigger asChild>
+          <button className="h-5 w-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 448 512"
+              className="fill-current bg-background text-foreground h-full w-full"
+            >
+              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </button>
+        </SheetTrigger>
+        <SheetContent side="top" className="p-10">
+          <SheetHeader className="text-left">
+            <SheetTitle>
+              <SheetClose asChild>
+                <Link href="#gaut">&lt;ggaut.dev&gt;</Link>
+              </SheetClose>
+            </SheetTitle>
+            <hr />
+          </SheetHeader>
+          <div className="flex flex-col gap-2 pt-2">
+            <SheetClose asChild>
+              <Link href="#about">&lt;/about&gt;</Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="#projects">&lt;/projects&gt;</Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href="#contact">&lt;/contact&gt;</Link>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <div>
+        <Button variant="ghost" size="icon">
+          <Link target="_blank" href="https://github.com/gautchap">
+            <Icons.gitHub className="h-[1.2rem] w-[1.2rem]" />
+          </Link>
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Link
+            target="_blank"
+            href="https://www.linkedin.com/in/gauthier-chapon"
+          >
+            <Icons.linkedin className="h-[1.2rem] w-[1.2rem]" />
+          </Link>
+        </Button>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
 }
