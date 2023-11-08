599d541cfe964c44a7f1f87badfa2b23b8d83dd8
gautchap <gautchap+github@outlook.com>
2023-11-08 01:35:45 +0100

    feat : create about section with optimized render

---
+++ src/components/about-section.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ReactNode, useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
+const cardVariants = {
+  initial: { opacity: 0, translateY: 15 },
+  animate: { opacity: 1, translateY: 0 },
+};
+
+type AboutSectionProps = {
+  children: ReactNode;
+};
+
+export default function AboutSection({ children }: AboutSectionProps) {
+  const reference = useRef(null);
+  const isInView = useInView(reference, { once: false });
+  return (
+    <motion.section
+      id="about"
+      variants={cardVariants}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+      transition={{ duration: 0.5 }}
+      className="h-[100dvh] pt-12 md:pt-44 container"
+      ref={reference}
+    >
+      {children}
+    </motion.section>
+  );
+}
