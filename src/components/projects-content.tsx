767781c6d8304d4193820b5ba4b4d064d44e51c9
gautchap <gautchap+github@outlook.com>
2023-11-08 01:35:57 +0100

    feat : create projects section with optimized render

---
+++ src/components/projects-content.tsx
@@ -0,0 +1,25 @@
+export default function ProjectsContent() {
+  return (
+    <>
+      <h2 className="font-bold text-3xl leading-none tracking-tight">
+        Projects
+      </h2>
+      <p>
+        I am a full stack web developer with a passion for creating interactive
+        and responsive web applications. I have experience working with
+        JavaScript, React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML,
+        CSS, and Git. I am a quick learner and I am always looking to expand my
+        knowledge and skill set. I am a team player and I am excited to work
+        with others to create amazing applications.
+      </p>
+      <span>Skills</span>
+      <ul>
+        <li>TypeScript</li>
+        <li>Node.js</li>
+        <li>PostgreSQL</li>
+        <li>Next.js</li>
+        <li>React</li>
+      </ul>
+    </>
+  );
+}
---
+++ src/components/projects-section.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { ReactNode, useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
+const cardVariants = {
+  initial: { opacity: 0, translateY: 15 },
+  animate: { opacity: 1, translateY: 0 },
+};
+
+type ProjectsSectionProps = {
+  children: ReactNode;
+};
+
+export default function ProjectsSection({ children }: ProjectsSectionProps) {
+  const reference = useRef(null);
+  const isInView = useInView(reference, { once: false });
+  return (
+    <motion.section
+      id="projects"
+      variants={cardVariants}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+      transition={{ duration: 0.5 }}
+      className="h-[100dvh] pt-12 md:pt-44 container"
+      ref={reference}
+    >
+      {children}
+    </motion.section>
+  );
+}
