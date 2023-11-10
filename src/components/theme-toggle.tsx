d9a73c4c44d702a048c9339187ad3ef82a66ec02
gautchap <gautchap+github@outlook.com>
2023-11-10 22:24:17 +0100

    refactor : fix ui error

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -5,28 +5,17 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <div className="relative">
-      <Button
-        title="switch theme dark"
-        onClick={() => setTheme("dark")}
-        variant="ghost"
-        size="icon"
-        className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      >
-        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-      <Button
-        title="switch theme white"
-        onClick={() => setTheme("light")}
-        variant="ghost"
-        size="icon"
-        className="absolute left-0 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      >
-        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-    </div>
+    <Button
+      title="switch theme white"
+      onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+      variant="ghost"
+      size="icon"
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="h-[1.2rem] w-[1.2rem] absolute  rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
   );
 }
