bf223c89cf7bb94a02c2a10d8c05d6a3bd31d80a
gautchap <gautchap+github@outlook.com>
2023-11-10 22:09:59 +0100

    refactor : fix ui error

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -8,15 +8,25 @@ export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <Button variant="ghost" size="icon">
-      <SunIcon
+    <div className="relative">
+      <Button
+        title="switch theme dark"
         onClick={() => setTheme("dark")}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <MoonIcon
+        variant="ghost"
+        size="icon"
+        className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      >
+        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+      </Button>
+      <Button
+        title="switch theme white"
         onClick={() => setTheme("light")}
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
-    </Button>
+        variant="ghost"
+        size="icon"
+        className="absolute left-0 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      >
+        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+      </Button>
+    </div>
   );
 }
