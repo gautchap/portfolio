5339752dbe74230a6a6d03665201bb7bdaa7491e
gautchap <gautchap+github@outlook.com>
2023-11-15 17:45:07 +0100

    refactor : export spring animation and change css

---
+++ src/components/animation.ts
@@ -0,0 +1,4 @@
+export const spring = {
+  initial: { opacity: 0, translateY: 15 },
+  animate: { opacity: 1, translateY: 0 },
+};
--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -2,11 +2,7 @@
 
 import { ReactNode, useRef } from "react";
 import { motion, useInView } from "framer-motion";
-
-const cardVariants = {
-  initial: { opacity: 0, translateY: 15 },
-  animate: { opacity: 1, translateY: 0 },
-};
+import { spring } from "@/components/animation";
 
 type ContactSectionProps = {
   children: ReactNode;
@@ -17,12 +13,12 @@ export default function ContactSection({ children }: ContactSectionProps) {
   const isInView = useInView(reference, { once: false });
   return (
     <motion.section
-      variants={cardVariants}
+      variants={spring}
       initial="initial"
       animate={isInView ? "animate" : "initial"}
       transition={{ duration: 0.5 }}
       id="contact"
-      className="pt-2 md:pt-44 container flex gap-4 flex-wrap md:flex-nowrap justify-center items-baseline transform-gpu"
+      className="pt-2 md:pt-44 container transform-gpu"
       ref={reference}
     >
       {children}
