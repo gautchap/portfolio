cb15ac47211cf0d57c0dec007af6f22f479f10cf
gautchap <gautchap+github@outlook.com>
2023-11-15 17:41:47 +0100

    feat : list of skills

---
+++ src/data/skills.ts
@@ -0,0 +1,18 @@
+export const skills = [
+  {
+    category: "Languages",
+    listSkills: ["HTML", "CSS", "JavaScript", "TypeScript"],
+  },
+  {
+    category: "Frontend",
+    listSkills: ["React", "Nextjs"],
+  },
+  {
+    category: "Design",
+    listSkills: ["CSS", "SASS", "Tailwind", "Styled"],
+  },
+  {
+    category: "Backend",
+    listSkills: ["Nodejs", "PostgreSQL", "MongoDB", "Prisma"],
+  },
+];
