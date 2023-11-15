0f6a0466c6515f96cff6aa7f5eec0339f6644acf
gautchap <gautchap+github@outlook.com>
2023-11-15 17:45:32 +0100

    refactor : export card animation

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -20,7 +20,7 @@ const heroVariants = {
   animate: { opacity: 1, scale: 1 },
 };
 
-const cardVariants = {
+export const cardVariants = {
   initial: { opacity: 0, translateY: 15 },
   animate: { opacity: 1, translateY: 0 },
 };
