c5ddfef0b4b44cd2c16fd67f6e3d87dcf237bd1b
gautchap <gautchap+github@outlook.com>
2023-11-15 17:46:14 +0100

    refactor : change css

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,21 +11,26 @@ import { ContactForm } from "@/components/contact-form";
 export default function Page() {
   return (
     <ContactSection>
-      <Card className="md:max-w-[20rem]">
-        <CardHeader>
-          <CardTitle>&gt;_ Let&apos;s Connect</CardTitle>
-          <CardDescription>
-            I&apos;m currently looking for new opportunities, my inbox is always
-            open. Whether you have a question or just want to say hi, I&apos;ll
-            try my best to get back to you!
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card className="w-full md:max-w-[55rem] shadow-md">
-        <CardContent className="p-5">
-          <ContactForm />
-        </CardContent>
-      </Card>
+      <h1 className="font-bold text-3xl leading-none tracking-tight mb-4 md:mb-8">
+        Contact
+      </h1>
+      <div className="flex gap-3 flex-wrap md:flex-nowrap justify-between items-baseline">
+        <Card className="md:max-w-[20rem]">
+          <CardHeader>
+            <CardTitle>&gt;_ Let&apos;s Connect</CardTitle>
+            <CardDescription>
+              I&apos;m currently looking for new opportunities, my inbox is
+              always open. Whether you have a question or just want to say hi,
+              I&apos;ll try my best to get back to you!
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="w-full md:max-w-[55rem] shadow-md">
+          <CardContent className="p-5">
+            <ContactForm />
+          </CardContent>
+        </Card>
+      </div>
     </ContactSection>
   );
 }
