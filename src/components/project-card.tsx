a6f486b6814474e9b60a1ea42c51f1d7dbff32c3
gautchap <gautchap+github@outlook.com>
2024-01-02 14:15:41 +0100

    refactor : change ui

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,49 +1,58 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { Project } from "@/data/projects";
 import Image from "next/image";
 
 type ProjectCardProps = {
-    project: Project;
+  project: Project;
 };
 
 export default function ProjectCard({ project }: ProjectCardProps) {
-    return (
-        <Link
-            href={project.url}
-            target="_blank"
-            className="inline-flex relative max-w-[320px] md:max-w-md aspect-video flex-wrap my-4 md:m-0 border rounded-xl md:border-none"
-        >
-            <div className="opacity-1 hover:opacity-20 transition-opacity duration-300">
-                <Image
-                    priority
-                    alt={project.name}
-                    className="block dark:hidden md:absolute rounded-xl aspect-video hover:blur-sm transition duration-300 transform-gpu"
-                    src={project.img.light}
-                />
-                <Image
-                    priority
-                    alt={project.name}
-                    className="hidden dark:block md:absolute rounded-xl aspect-video hover:blur-sm transition duration-300 transform-gpu"
-                    src={project.img.dark}
-                />
-            </div>
-            <Card className="md:bg-black md:text-white border-none md:h-full">
-                <CardHeader>
-                    <CardTitle>&gt;_ {project.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <CardDescription className="md:text-white">{project.description}</CardDescription>
-                </CardContent>
-                <CardFooter className="flex-wrap gap-1">
-                    {project.badges.map((badge) => (
-                        <Badge key={badge} variant="secondary">
-                            {badge}
-                        </Badge>
-                    ))}
-                </CardFooter>
-            </Card>
-        </Link>
-    );
+  return (
+    <Link
+      href={project.url}
+      target="_blank"
+      className="inline-flex relative h-[30em] md:h-auto max-w-[320px] md:max-w-md aspect-video flex-wrap my-4 md:m-0 border rounded-xl md:border-none"
+    >
+      <div className="opacity-1 hover:opacity-20 transition-opacity duration-300 bg-gradient-to-b from-55% from-transparent to-background">
+        <Image
+          priority
+          alt={project.name}
+          className="z-[-1] relative block dark:hidden md:z-10 md:absolute rounded-b-none md:rounded-b-xl rounded-xl aspect-video hover:blur-sm transition duration-300 transform-gpu"
+          src={project.img.light}
+        />
+        <Image
+          priority
+          alt={project.name}
+          className="z-[-1] relative hidden dark:block md:z-10 md:absolute rounded-b-none md:rounded-b-xl rounded-xl aspect-video hover:blur-sm transition duration-300 transform-gpu"
+          src={project.img.dark}
+        />
+      </div>
+      <Card className="md:bg-black md:text-white border-none md:h-full">
+        <CardHeader>
+          <CardTitle>&gt;_ {project.name}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="md:text-white">
+            {project.description}
+          </CardDescription>
+        </CardContent>
+        <CardFooter className="flex-wrap gap-1">
+          {project.badges.map((badge) => (
+            <Badge key={badge} variant="secondary">
+              {badge}
+            </Badge>
+          ))}
+        </CardFooter>
+      </Card>
+    </Link>
+  );
 }
