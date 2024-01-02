7a170dfaa095dc0b0e83f5d5b901d03c13ba6ae4
gautchap <gautchap+github@outlook.com>
2024-01-02 15:00:56 +0100

    refactor : use gpu acceleration

--- src/components/hero-image.tsx
+++ src/components/hero-image.tsx
@@ -6,9 +6,8 @@ export default function HeroImage() {
     <div className="rounded-full bg-secondary w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
       <Image
         priority={true}
-        quality={100}
         src={hero}
-        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        className="absolute transform-gpu -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
         draggable="false"
         alt="computer"
         width={300}
