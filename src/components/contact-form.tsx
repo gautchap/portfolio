a25c5e310f3de84af9f3cb042c76be666255f10e
gautchap <gautchap+github@outlook.com>
2024-02-26 16:28:08 +0100

    refactor : use lucide icons for contact form and theme toggle

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -3,119 +3,102 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 import { toast } from "@/components/ui/use-toast";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
-import { ReloadIcon } from "@radix-ui/react-icons";
+import { RotateCw } from "lucide-react";
 import sendMail from "@/actions/send-mail";
 
 const formSchema = z.object({
-  mail: z.string().email().min(1),
-  subject: z.string().min(1).max(100),
-  message: z.string().min(10).max(1000),
+    mail: z.string().email().min(1),
+    subject: z.string().min(1).max(100),
+    message: z.string().min(10).max(1000),
 });
 
 type FormValues = z.infer<typeof formSchema>;
 
 export function ContactForm() {
-  const form = useForm<FormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      mail: "",
-      message: "",
-      subject: "",
-    },
-  });
-  async function onSubmit(values: FormValues) {
-    try {
-      await sendMail(values);
-      form.reset();
-      toast({
-        description: "✅ Your message has been sent.",
-      });
-    } catch {
-      toast({
-        variant: "destructive",
-        description: "❌ There was a problem with your request.",
-      });
+    const form = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            mail: "",
+            message: "",
+            subject: "",
+        },
+    });
+    async function onSubmit(values: FormValues) {
+        try {
+            await sendMail(values);
+            form.reset();
+            toast({
+                description: "✅ Your message has been sent.",
+            });
+        } catch {
+            toast({
+                variant: "destructive",
+                description: "❌ There was a problem with your request.",
+            });
+        }
     }
-  }
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="mail"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="mail@example.com"
-                  type="email"
-                  required
-                  {...field}
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                <FormField
+                    control={form.control}
+                    name="mail"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="mail@example.com" type="email" required {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="subject"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Subject</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Just a subject"
-                  type="text"
-                  required
-                  {...field}
+                <FormField
+                    control={form.control}
+                    name="subject"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Subject</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Just a subject" type="text" required {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="message"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Message</FormLabel>
-              <FormControl>
-                <Textarea
-                  rows={10}
-                  placeholder="Let's talk about..."
-                  required
-                  className="resize-none"
-                  {...field}
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Message</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    rows={10}
+                                    placeholder="Let's talk about..."
+                                    required
+                                    className="resize-none"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {form.formState.isSubmitting ? (
-          <Button disabled>
-            <ReloadIcon className="mr-2 size-4 animate-spin" />
-            Sending...
-          </Button>
-        ) : (
-          <Button type="submit">Send</Button>
-        )}
-      </form>
-    </Form>
-  );
+                {form.formState.isSubmitting ? (
+                    <Button disabled>
+                        <RotateCw className="mr-2 size-4 animate-spin" />
+                        Sending...
+                    </Button>
+                ) : (
+                    <Button type="submit">Send</Button>
+                )}
+            </form>
+        </Form>
+    );
 }
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
 
-  return (
-    <Button
-      title="switch theme white"
-      onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
-      variant="ghost"
-      size="icon"
-    >
-      <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute size-[1.2rem] rotate-90  scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    </Button>
-  );
+    return (
+        <Button
+            title="switch theme white"
+            onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+            variant="ghost"
+            size="icon"
+        >
+            <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+    );
 }
